3d8b1a1139c8eed1ec31aca4245c5fc82310a545
Milena Aguiar <milena.nsantos@outlook.com>
2023-12-27 16:46:20 -0300

    Variáveis do IMC

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,12 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista"
+
+var paciente = document.querySelector("#primeiro-paciente")
+
+var tdPeso = paciente.querySelector (".info-peso")
+var peso = tdPeso.textContent
+
+var tdAltura = paciente.querySelector(".info-altura")
+var altura = tdAltura.textContent
+
+console.log(altura)
