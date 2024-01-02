5cf43baa3156db87cd876b9ddf67edf0717fc16b
Milena Aguiar <milena.nsantos@outlook.com>
2024-01-02 16:25:09 -0300

    Calculo do IMC do primeiro paciente

--- js/principal.js
+++ js/principal.js
@@ -1,12 +1,18 @@
 var titulo = document.querySelector(".titulo");
-titulo.textContent = "Aparecida Nutricionista"
+titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector("#primeiro-paciente")
+var paciente = document.querySelector("#primeiro-paciente");
 
-var tdPeso = paciente.querySelector (".info-peso")
-var peso = tdPeso.textContent
+var tdPeso = paciente.querySelector (".info-peso");
+var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura")
-var altura = tdAltura.textContent
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
 
-console.log(altura)
+var tdImc = paciente.querySelector(".info-imc")
+
+var imc = peso / (altura * altura);
+
+tdImc.textContent = imc
+
+console.log (imc)
